1858ca1ba454a1b6acd3e0bf238909843851da7b
GowthamORA <gepparla@GEPPARLA-IN.in.oracle.com>
2020-04-16 00:31:29 +0530

    Minor Updates

--- src/test/java/testrunner/TestRunner.java
+++ src/test/java/testrunner/TestRunner.java
@@ -10,7 +10,7 @@ import io.cucumber.testng.CucumberOptions;
 				dryRun = false,
 				monochrome = true,
 				strict = true,
-				tags = {"@FULLRUN"})
+				tags = {"@FULLRUN or @SMOKE"})
 
 public class TestRunner extends AbstractTestNGCucumberTests{
 	
