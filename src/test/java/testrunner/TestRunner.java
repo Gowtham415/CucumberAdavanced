7561066497c4b21eb90bf4c57919ca65e3c6449c
GowthamORA <gepparla@GEPPARLA-IN.in.oracle.com>
2020-09-24 21:14:13 +0530

    tag Updates

--- src/test/java/testrunner/TestRunner.java
+++ src/test/java/testrunner/TestRunner.java
@@ -10,7 +10,7 @@ import io.cucumber.testng.CucumberOptions;
 				dryRun = false,
 				monochrome = true,
 				strict = true,
-				tags = {"@TESTRUN"})
+				tags = {"@SMOKE"})
 
 public class TestRunner extends AbstractTestNGCucumberTests{
 	
